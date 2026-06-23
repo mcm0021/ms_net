f042e39bd67714b7618a39b4001cba4418dc41f1
Moritz Münter <mmuenter3004@gmail.com>
2026-06-23 09:43:49 +0200

    full training update

--- full_training.py
+++ full_training.py
@@ -1,25 +1,25 @@
-import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from pathlib import Path
-
 import torch
 import json
 import urllib.request
 import numpy as np
-from scipy.ndimage import distance_transform_edt
 
+from scipy.ndimage import distance_transform_edt
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from hdf5storage import loadmat
+from pathlib import Path
 from torch import nn
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-from hdf5storage import loadmat
+from torch.utils.data import Dataset, DataLoader
 
 from network_tools import get_masks, calc_loss
 from network_tools import scale_tensor
 from network import MS_Net
 
 def preprocess_data(data, num_scales=2):
+  """
+  Transforms array to different scales as tensors and returns them as list. 
+  """
   # Convert data into torch tensor
   data = torch.as_tensor(data, dtype=torch.float32)
 
@@ -36,8 +36,34 @@ def preprocess_data(data, num_scales=2):
 
   return data
 
+class EarlyStopper:
+    """
+    Breaks if validation loss does not change anymore, to prevent overfitting. 
+    """
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
 
 class PorousMediaDataset(Dataset):
+    """
+    Dataset specified to data at https://web.corral.tacc.utexas.edu/digitalporousmedia/DRP-372/.
+    Images and simulations in root_dir.
+    In init step for all images the euclidiean distance is calculated.
+    Those and their corresponding simulations are preprocessed (scaled).
+    Tasks are calculated and everything is loaded to RAM. 
+    """
     def __init__(self, root_dir: str, scales: int, transform=None):
         self.root_dir = Path(root_dir)
         self.scales = scales
@@ -53,31 +79,26 @@ class PorousMediaDataset(Dataset):
             if not dir_path.is_dir():
                 continue
 
-            lbm_path = dir_path / "LBM.mat"
-            mat_path = dir_path / f"{dir_path.name}.mat"
+            simulation_path = dir_path / "LBM.mat"
+            image_path = dir_path / f"{dir_path.name}.mat"
 
-            if lbm_path.exists() and mat_path.exists():
-                self.sim_image_paths.append(str(lbm_path))
-                self.image_paths.append(str(mat_path))
-
-        # self.image_paths = self.image_paths[:5]
-        # self.sim_image_paths = self.sim_image_paths[:5]
-
+            if simulation_path.exists() and image_path.exists():
+                self.sim_image_paths.append(str(simulation_path))
+                self.image_paths.append(str(image_path))
+                
         max_sim = 1e-8
         for sim_path in self.sim_image_paths:
             simulation = loadmat(sim_path)['uz']
             current_max = np.max(np.abs(simulation))
             if current_max > max_sim:
                 max_sim = current_max
-
+        
         temp_images = []
         temp_edist = []
         temp_masks = []
         temp_sims = []
-        count = 0
+
         for img_path, sim_path in zip(self.image_paths, self.sim_image_paths):
-            print(count)
-            count += 1
             image = loadmat(img_path)['bin']
             simulation = loadmat(sim_path)['uz']
 
@@ -120,12 +141,16 @@ class PorousMediaDataset(Dataset):
         return image, edist_sample, mask_sample, simulation
 
 def download_data(root_dir: str, max_workers: int = 5):
+    """
+    Downloads all 256x256x256 images with their LBM simulations from "https://web.corral.tacc.utexas.edu/digitalporousmedia/DRP-372/". 
+    Metadata file "DRP-372_metadata.json" necessary. 
+    """
 
     base_url = "https://web.corral.tacc.utexas.edu/digitalporousmedia/DRP-372/"
     root_path = Path(root_dir)
     root_path.mkdir(parents=True, exist_ok=True)
 
-    with open('DRP-372_metadata (1).json') as f:
+    with open('DRP-372_metadata.json') as f:
         metadata = json.load(f)
 
     filtered_data = [node for node in metadata["nodes"] if node.get("label") and "Single Phase" in node["label"] and "480" not in node["label"]]
@@ -156,8 +181,7 @@ def download_data(root_dir: str, max_workers: int = 5):
             download_tasks.append((f'{base_url}{safe_path_lbm}', target_dir / "LBM.mat"))
 
             download_tasks.append((f'{base_url}{mat_file_obj["path"]}', target_dir / mat_filename))
-    download_tasks = download_tasks[:10]
-    print(len(download_tasks))
+
     completed_count = 0
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = {executor.submit(download_file, url, path): path for url, path in download_tasks}
@@ -169,6 +193,9 @@ def download_data(root_dir: str, max_workers: int = 5):
 
 
 def download_file(url: str, target_path: Path) -> str:
+    """
+    Download single file to obtain concurrency. 
+    """
     if target_path.exists():
         return f"Skipped (already exists): {target_path.name}"
 
@@ -180,7 +207,10 @@ def download_file(url: str, target_path: Path) -> str:
 
 def train(model: MS_Net,
           train_dataloader, val_dataloader,
-          optimizer, scheduler, num_epochs, loss_function, accumulation_steps):
+          optimizer, scheduler, num_epochs, loss_function, accumulation_steps, early_stopper: EarlyStopper=None):
+    """
+    Training of MS_Net with mini batch gradient descent. 
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
     model = model.to(device)
@@ -189,7 +219,6 @@ def train(model: MS_Net,
     val_loss_logs = []
 
     for epoch in range(num_epochs):
-        print(epoch)
         model.train()
         epoch_loss = 0
         optimizer.zero_grad()
@@ -203,22 +232,37 @@ def train(model: MS_Net,
             y_hat = model(x_sample, mask_sample)
 
             batch_loss, _ = calc_loss(y_hat, y_sample, loss_function, {})
-            print(batch_loss)
             scaled_loss = batch_loss / accumulation_steps
             scaled_loss.backward()
 
             epoch_loss += batch_loss.item()
 
             if (batch_idx + 1) % accumulation_steps == 0 or (batch_idx + 1) == len(train_dataloader):
-                print(epoch_loss)
                 optimizer.step()
                 optimizer.zero_grad()
 
         scheduler.step(epoch_loss)
 
+        if early_stopper is not None:
+            model.eval()
+            val_loss = 0
+
+            with torch.no_grad():
+                for batch_idx, (_, image_scales, masks, simulation) in enumerate(val_dataloader):
+                    x_sample = [scale.squeeze(0).to(device) for scale in image_scales]
+                    mask_sample = [scale.squeeze(0).to(device) for scale in masks]
+                    y_sample = [scale.squeeze(0).to(device) for scale in simulation]
+
+                    y_hat = model(x_sample, mask_sample)
+                    loss, _ = calc_loss(y_hat, y_sample, loss_function, {})
+                    val_loss += loss.item()
+
+            arg_val_loss = val_loss / len(val_dataloader)
+            if early_stopper.early_stop(arg_val_loss):
+                break
+
         if epoch % 100 == 0:
-            model.save_as("test")
-            print(epoch_loss)
+            model.save_model()
             loss_logs.append(epoch_loss)
 
             model.eval()
@@ -235,35 +279,33 @@ def train(model: MS_Net,
                     val_loss += loss.item()
 
             arg_val_loss = val_loss / len(val_dataloader)
-            print(arg_val_loss)
             val_loss_logs.append(arg_val_loss)
+            print(f'Epoch {epoch + 1}/{epochs} | Val Loss: {arg_val_loss}')
 
     model.save_model()
     torch.save(model.state_dict(), 'savedModels/model_weights.pth')
     return loss_logs, val_loss_logs
 
-net = MS_Net(
-              num_scales   := 4,   # num of trainable convNets
-              num_features  = 1,   # input features (Euclidean distance, etc)
-              num_filters   = 2,   # num of kernels on each layer of the finest model (most expensive)
-              summary       = False # print the model summary
-)
-print(torch.cuda.is_available())
-download_data("data")
+if __name__ == '__main__':
+    net = MS_Net(
+                num_scales   := 4,   # num of trainable convNets
+                num_features  = 1,   # input features (Euclidean distance, etc)
+                num_filters   = 2,   # num of kernels on each layer of the finest model (most expensive)
+                summary       = False # print the model summary
+    )
+    download_data("data")     
 
-data = PorousMediaDataset("data", num_scales)
-train_data, test_data = torch.utils.data.random_split(data, [0.8, 0.2])
-train_loader = DataLoader(train_data, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_data, batch_size=1, shuffle=True)
-print(len(train_loader))
+    data = PorousMediaDataset("data", num_scales)
+    train_data, test_data = torch.utils.data.random_split(data, [0.8, 0.2])
 
-learning_rate = 1e-5
-epochs = 2500
-EFFECTIVE_BATCH_SIZE = 4
-optimizer = Adam(net.parameters(), lr=learning_rate)
-scheduler = ReduceLROnPlateau(optimizer, threshold=1e-6, min_lr=1e-6, patience=25)
-loss_f = nn.MSELoss()
-loss_logs, val_logs = train(net, train_loader, test_loader, optimizer, scheduler, epochs, learning_rate, loss_f, EFFECTIVE_BATCH_SIZE)
+    train_loader = DataLoader(train_data, batch_size=1, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=True)
 
-print(loss_logs)
-print(val_logs)
+    learning_rate = 1e-5
+    epochs = 2500
+    EFFECTIVE_BATCH_SIZE = 4
+    optimizer = Adam(net.parameters(), lr=learning_rate)
+    scheduler = ReduceLROnPlateau(optimizer, threshold=1e-6, min_lr=1e-6, patience=25)
+    early_stopper = EarlyStopper(5, 1e-4)
+    loss_f = nn.MSELoss()
+    loss_logs, val_logs = train(net, train_loader, test_loader, optimizer, scheduler, epochs, loss_f, EFFECTIVE_BATCH_SIZE, early_stopper)
